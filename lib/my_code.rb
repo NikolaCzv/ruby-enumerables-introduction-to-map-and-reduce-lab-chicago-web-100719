4a0badda7b9b036ed97f8c9380527d0da1f7dd09
Nikola Raicic <nikola.raicic@hotmail.com>
2019-09-29 22:08:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,8 +44,8 @@ end
 def reduce_to_total(source_array, starting_point = 0)
 new = starting_point
 i = 0 
-while i < starting_point.length do 
-  new += 
+while i < source_array.length do 
+  new += source_array[i]
 
 
 
